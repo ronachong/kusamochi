a164418412ded365eb4020e5f6f1c2c7bc8e6755
Rona Chong <ronachong@gmail.com>
2017-08-12 20:49:58 -0700

    Update front-end to serve example React app.

---
+++ docker-frontend/express.js
@@ -0,0 +1,14 @@
+const express = require('express');
+const path = require('path');
+const app = express();
+const build_path = path.join(__dirname, 'react-app/build');
+
+app.use(express.static(build_path));
+
+app.get('/*', function(req, res) {
+    res.sendFile(build_path);
+});
+
+app.listen(3000, function(){
+    console.log('Example app listening on port 3000!');
+});
