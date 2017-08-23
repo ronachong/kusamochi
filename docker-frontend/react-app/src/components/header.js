f999308fd2d06bb19c55a756dfddd70fca079ba3
Rona Chong <ronachong@gmail.com>
2017-08-22 18:20:20 -0700

    Begin styling header with Aphrodite.

--- docker-frontend/react-app/src/components/header.js
+++ docker-frontend/react-app/src/components/header.js
@@ -1,6 +1,46 @@
 import React, { Component } from 'react';
-import logo from '../logo.svg';
 import '../App.css';
+
+import { StyleSheet, css } from 'aphrodite';
+
+
+const colors = {
+  ksm_green_lite: '205,235,142',
+  ksm_green_med: '165,201,86'
+}
+
+const gradients = {
+  ksm_green: `top, rgba(234,234,234,0) 70%, rgba(234,234,234,0.13) 84%, rgba(${colors.ksm_green_lite},0.77) 98%, rgba(${colors.ksm_green_lite},1) 100%`
+}
+
+const fonts = {
+  heading_enc: "'Encode Sans', sans-serif"
+}
+
+const styles = StyleSheet.create({
+  headerLink: {
+    textDecoration: 'none'
+  },
+
+  header: {
+    display: 'flex',
+    flexDirection: 'column',
+    background: `-moz-linear-gradient(${gradients.ksm_green})`, /* FF3.6-15 */
+    background: `-webkit-linear-gradient(${gradients.ksm_green})`, /* Chrome10-25,Safari5.1-6 */
+    background: `linear-gradient(${gradients.ksm_green})`, /* W3C, IE10+, FF16+, Chrome26+, Opera12+, Safari7+ */
+    filter: "progid:DXImageTransform.Microsoft.gradient( startColorstr='#00eaeaea', endColorstr='#a5c956',GradientType=0 )", /* IE6-9 */
+    //backgroundColor: 'white',
+    color: 'green'
+  },
+
+  headerTitle: {
+    color: `rgb(${colors.ksm_green_med})`,
+    fontFamily: fonts.heading_enc,
+    letterSpacing: '.3em',
+    padding: '2vh'
+  }
+
+})
 
 class Header extends Component {
   constructor(props) {
@@ -25,16 +65,16 @@ class Header extends Component {
   render() {
     return (
       <a
+        className={css(styles.headerLink)}
         href='#'
         onMouseEnter={() => this.mouseEnter()}
         onMouseLeave={() => this.mouseLeave()}
         onClick={() => this.onClick()}
       >
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
+        <header className={css(styles.header)}>
           <br />
           <label>Site Nav</label>
-          <h1>{this.props.title}</h1>
+          <h1 className={css(styles.headerTitle)}>{this.props.title}</h1>
         </header>
       </a>
     );
