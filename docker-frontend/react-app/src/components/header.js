51db9dcdd2d9bb3a60721c1c6dbad5389101986d
Rona Chong <ronachong@gmail.com>
2017-08-22 18:40:31 -0700

    Style header subtitle and adjust proportions of header.

--- docker-frontend/react-app/src/components/header.js
+++ docker-frontend/react-app/src/components/header.js
@@ -6,15 +6,18 @@ import { StyleSheet, css } from 'aphrodite';
 
 const colors = {
   ksm_green_lite: '205,235,142',
-  ksm_green_med: '165,201,86'
+  ksm_green_med: '165,201,86',
+  blue_gray: '63,76,107',
+  black: '#000'
 }
 
 const gradients = {
-  ksm_green: `top, rgba(234,234,234,0) 70%, rgba(234,234,234,0.13) 84%, rgba(${colors.ksm_green_lite},0.77) 98%, rgba(${colors.ksm_green_lite},1) 100%`
+  ksm_green: `top, rgba(234,234,234,0) 70%, rgba(234,234,234,0.13) 87%, rgba(${colors.ksm_green_lite},0.5) 98%, rgba(${colors.ksm_green_lite},1) 100%`
 }
 
 const fonts = {
-  heading_enc: "'Encode Sans', sans-serif"
+  heading_enc: "'Encode Sans', sans-serif",
+  heading_slab: "'Roboto Slab', serif"
 }
 
 const styles = StyleSheet.create({
@@ -29,15 +32,20 @@ const styles = StyleSheet.create({
     background: `-webkit-linear-gradient(${gradients.ksm_green})`, /* Chrome10-25,Safari5.1-6 */
     background: `linear-gradient(${gradients.ksm_green})`, /* W3C, IE10+, FF16+, Chrome26+, Opera12+, Safari7+ */
     filter: "progid:DXImageTransform.Microsoft.gradient( startColorstr='#00eaeaea', endColorstr='#a5c956',GradientType=0 )", /* IE6-9 */
-    //backgroundColor: 'white',
-    color: 'green'
   },
 
   headerTitle: {
     color: `rgb(${colors.ksm_green_med})`,
     fontFamily: fonts.heading_enc,
+    fontSize: '3.5vh',
     letterSpacing: '.3em',
-    padding: '2vh'
+    padding: '0 2vh 1vh 2vh'
+  },
+
+  headerSubTitle: {
+    color: `rgb(${colors.blue_gray})`,
+    fontFamily: fonts.heading_slab,
+    fontSize: '1.4vh'
   }
 
 })
@@ -73,7 +81,7 @@ class Header extends Component {
       >
         <header className={css(styles.header)}>
           <br />
-          <label>Site Nav</label>
+          <label className={css(styles.headerSubTitle)}>- Site Nav -</label>
           <h1 className={css(styles.headerTitle)}>{this.props.title}</h1>
         </header>
       </a>
