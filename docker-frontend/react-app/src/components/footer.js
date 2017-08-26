9d34387e1565b3fa01c85471918ac107b6f09387
Rona Chong <ronachong@gmail.com>
2017-08-25 17:06:02 -0700

    Convert footer into functional component.
    Simplifies code and improves performance.

--- docker-frontend/react-app/src/components/footer.js
+++ docker-frontend/react-app/src/components/footer.js
@@ -2,21 +2,11 @@ import React, { Component } from 'react';
 import logo from '../logo.svg';
 import '../App.css';
 
-class Footer extends Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-    };
-  }
-  render() {
-    console.log(this.props);
-    return (
-      <footer className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>Some text here</p>
-      </footer>
-    );
-  }
-}
+const Footer = () => (
+  <footer className="App-header">
+    <img src={logo} className="App-logo" alt="logo" />
+    <p>Some text here</p>
+  </footer>
+)
 
 export default Footer;
